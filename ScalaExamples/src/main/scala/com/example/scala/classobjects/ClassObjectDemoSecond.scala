399ac40815c5f9f0b262f2defd458435a8470ec1
Selvaraj Jayaraman <techselva87@gmail.com>
2020-04-19 18:06:02 +0530

    Class Objects example two

---
+++ ScalaExamples/src/main/scala/com/example/scala/classobjects/ClassObjectDemoSecond.scala
@@ -0,0 +1,11 @@
+package com.example.scala.classobjects
+
+object ClassObjectDemoSecond {
+  
+   def main(args: Array[String]){
+     
+     val loc = new Location(10,20,15);
+     
+     loc.move(10, 10, 5);
+   }
+}
