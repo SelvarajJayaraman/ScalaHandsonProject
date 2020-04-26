8c84cfcee2c12d77d286137a3291bb71323c2851
Selvaraj Jayaraman <techselva87@gmail.com>
2020-04-26 12:13:46 +0530

    For Loop statement example

---
+++ ScalaExamples/src/main/scala/com/example/scala/ForLoopStatment.scala
@@ -0,0 +1,33 @@
+package com.example.scala
+
+object ForLoopStatment {
+  
+  def main(args: Array[String]){
+    
+    for( i <- 1 to 6){
+      println(" i using to :" + i);
+    }
+    
+    for ( i <- 1 until 6){
+      println("i using until :" + i);
+    }
+    
+    val lst = List(1,2,3,4,6,8,9);
+    for( i <- lst){
+      println("using list:"+i);
+    }
+    
+    for( i <- lst; if i < 6){
+      println("using filter:" + i);
+    }
+    
+    var result =  for { i <- lst
+      if i < 6 } yield {
+        i * i;
+    } 
+    println(result);
+    
+  }
+  
+  
+}
