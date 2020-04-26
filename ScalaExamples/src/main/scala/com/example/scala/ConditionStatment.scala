bd72b5dac5659a57a7b674f247f200193cb9d2d3
Selvaraj Jayaraman <techselva87@gmail.com>
2020-04-26 11:07:42 +0530

    Scala condition statment example

---
+++ ScalaExamples/src/main/scala/com/example/scala/ConditionStatment.scala
@@ -0,0 +1,20 @@
+package com.example.scala
+
+object ConditionStatment {
+  
+  def main(args: Array[String]){
+     val x = 20;
+     var result1 = "";
+     
+     if(x == 20){
+       result1 = "X = 20";
+     }else {
+       result1 = "X !=20";
+     }
+     
+     println(result1);
+     
+     var result2 = if( x!=20) "X = 20" else "X !=20";
+     println(result2);
+  }
+}
