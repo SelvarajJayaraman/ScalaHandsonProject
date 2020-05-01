cf23694173ebef594048bb8aa964fc2a54aee45a
Selvaraj Jayaraman <techselva87@gmail.com>
2020-05-01 22:20:36 +0530

    Scala Maps example

---
+++ ScalaExamples/src/main/scala/com/example/scala/collections/MapExample.scala
@@ -0,0 +1,29 @@
+package com.example.scala.collections
+
+object MapExample {
+  
+  val mymap1: Map[Int,String] = Map(100 -> "selva",101 -> "Raj", 102-> "kumar");
+  val mymap2: Map[Int,String] = Map(103 -> "bala");
+  
+  def main(args: Array[String]){
+    
+    println("Print raw map:" + mymap1);
+    
+    println("Get the Specific value using key: "+ mymap1(101));
+    
+    println("Print all keys in map:" + mymap1.keys);
+    
+    println("Print all values in map:" + mymap1.values);
+    
+    println("Check map is empty:" + mymap1.isEmpty);
+    
+    //Iterate and print key and values from map.
+    mymap1.keys.foreach { key =>
+      println("Key:" + key);
+      println("Value:" + mymap1(key));
+    }
+    
+    //Concat two maps.
+    println(mymap1 ++ mymap2);
+  }
+}
