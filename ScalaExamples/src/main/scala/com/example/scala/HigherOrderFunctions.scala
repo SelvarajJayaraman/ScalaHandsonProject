22774511db8bd6407bd4b3b35f877e85bd595212
Selvaraj Jayaraman <techselva87@gmail.com>
2020-05-01 12:57:46 +0530

    Higher Order functions example

---
+++ ScalaExamples/src/main/scala/com/example/scala/HigherOrderFunctions.scala
@@ -0,0 +1,21 @@
+package com.example.scala
+
+object HigherOrderFunctions {
+  
+  def math1(x: Double, y:Double, f:(Double,Double) => Double) : Double = f(x,y);
+  
+  def math2(x:Double, y:Double, z:Double, f:(Double,Double) => Double) : Double = f(f(x,y),z);
+  
+  def main(args: Array[String]) {
+    
+     var result1 = math1(50,20,(x,y) => x min y);
+     println(result1);
+     
+     var result2 = math2(30,40, 10, (x,y) => x * y);
+     println(result2);
+     
+     //Wildcare function notation
+     var result3 = math1(10,20, _+_);
+     println(result3);
+  }
+}
